ef136edc3b8e139fe92e13ce6d850c37d1e17f4a
Hair Salon Developer <developer@hairsalon.com>
2025-11-27 10:03:38 +0700

    logic nhấn vào check box

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -4,6 +4,30 @@ document.querySelector('.sidebar-toggle').addEventListener('click', function() {
 });
 // End Sidebar toggle for mobile
 
+// Check box change multi
+const checkBoxAll = document.querySelector("input[name='check-all']");
+if (checkBoxAll) {
+    checkBoxAll.addEventListener("change", e => {
+        const checkBoxs = document.querySelectorAll("input[name='check']");
+        checkBoxs.forEach(e => {
+            e.checked =(e.checked == true ? false : true);
+        })
+    })
+}
 
 
-
+const checkBoxs = document.querySelectorAll("input[name='check']");
+if (checkBoxs) {
+    checkBoxs.forEach( checkBox => {
+        checkBox.addEventListener("change", e => {
+            e.checked =(e.checked == true ? false : true);
+            const checBoxsChecked = document.querySelectorAll("input[name='check']:checked");
+            if (checkBoxs.length != checBoxsChecked.length) {
+                checkBoxAll.checked = false;
+            } else {
+                checkBoxAll.checked = true;
+            }
+        });
+    });
+}
+// End Check box change multi
