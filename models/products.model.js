775693f6388a2a21a140cc0e6bc9c2a51ad3f3fc
Hair Salon Developer <developer@hairsalon.com>
2025-12-02 22:05:04 +0700

    timestamp = true in products.model

--- models/products.model.js
+++ models/products.model.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const ProductsSchema = new Schema({
-    title: String,
-    description: String,
-    price: Number,
-    discountPercentage: Number,
-    stock: Number,
-    thumbnail: String,
-    status: String,
-    position: Number,
-    feature: {
-        type: String,
-        default: ""
-    },
-    deleted: {
-        type: Boolean,
-        default: false
+const ProductsSchema = new Schema(
+    {
+        title: String,
+        description: String,
+        price: Number,
+        discountPercentage: Number,
+        stock: Number,
+        thumbnail: String,
+        status: String,
+        position: Number,
+        feature: {
+            type: String,
+            default: ""
+        },
+        deleted: {
+            type: Boolean,
+            default: false
+        }
+    }, {
+        timestamps: true
     }
-    
-});
+);
 const Products = mongoose.model('Products', ProductsSchema, "products");
 
 module.exports = Products;
