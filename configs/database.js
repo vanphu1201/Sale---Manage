5a0c6331415691c8fc7346c27dc09744cc210313
Hair Salon Developer <developer@hairsalon.com>
2025-12-02 23:11:48 +0700

    kết nối mongo online

--- configs/database.js
+++ configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
     try {
-      await mongoose.connect("mongodb://127.0.0.1:27017/sale-manage");
+      await mongoose.connect("mongodb+srv://vanphu120107_db_user:b6w35IFnkbs41KUJ@sale-mangage.igqhcm8.mongodb.net/sale-manage");
       console.log("Connected to MongoDB");
     } catch (error) {
       console.error("MongoDB connection error:", error);
