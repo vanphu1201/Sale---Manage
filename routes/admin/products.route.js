44f920de4a0bb958a579379634c49c8d771e6344
Hair Salon Developer <developer@hairsalon.com>
2025-12-03 23:19:11 +0700

    tối ưu upload ảnh lên cloudinary

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const route = express.Router();
-const cloudinary = require('cloudinary').v2;
-const streamifier = require('streamifier');
+
 const multer  = require('multer');
 const upload = multer();
 
-// Cloudinary
-cloudinary.config({ 
-  cloud_name: 'dbhfynfvh', 
-  api_key: '824913551957631', 
-  api_secret: '_drih_esMy2RqlFkMYQXM5IHYMw'
-});
-// End Cloudinary
+const uploadClould = require("../../midlewares/admin/uploadClould.middleware");
+
 
 const controller = require("../../controllers/admin/products.controller");
 
@@ -27,36 +21,7 @@ route.get("/products/create", controller.create);
 route.post(
     "/products/create",
     upload.single('thumbnail'),
-    function (req, res, next) {
-        if (req.file) {
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                        (error, result) => {
-                            if (result) {
-                                resolve(result);
-                            } else {
-                                reject(error);
-                            }
-                        }
-                    );
-
-                    streamifier.createReadStream(req.file.buffer).pipe(stream);
-                });
-            };
-
-            async function upload(req) {
-                let result = await streamUpload(req);
-                req.body[req.file.fieldname] = result.secure_url;
-                next();
-            }
-
-            upload(req);
-        } else {
-            next();
-        }
-        
-    },
+    uploadClould.upload,
     controller.createPost
 );
 
@@ -65,35 +30,7 @@ route.get("/products/edit/:id", controller.edit);
 route.post(
     "/products/edit/:id",
     upload.single('thumbnail'),
-    function (req, res, next) {
-        if (req.file) {
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                        (error, result) => {
-                            if (result) {
-                                resolve(result);
-                            } else {
-                                reject(error);
-                            }
-                        }
-                    );
-
-                    streamifier.createReadStream(req.file.buffer).pipe(stream);
-                });
-            };
-
-            async function upload(req) {
-                let result = await streamUpload(req);
-                req.body[req.file.fieldname] = result.secure_url;
-                next();
-            }
-
-            upload(req);
-        } else {
-            next();
-        }
-    },
+    uploadClould.upload,
     controller.editPost
 );
 
