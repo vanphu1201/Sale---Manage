a23a749a7b6b0d3e34e10140fc3d21721bb78942
Hair Salon Developer <developer@hairsalon.com>
2025-12-05 22:45:17 +0700

    đệ quy phân cấp danh mục trong phần chọn danh muc ở category.create

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -17,12 +17,27 @@ module.exports.index = async (req, res) => {
 module.exports.create = async (req, res) => {
     const id = (req.params.id == "new" ? "" : req.params.id);
     const categories = await Category.find({deleted: false});
-    let categoriesTitle = [];
-    categoriesTitle = categories.map(item => item.title);
-    console.log(categoriesTitle)
+
+    function createTree(arr, parentId = "") {
+        const tree = [];
+        arr.forEach((item) => {
+            if (item.parent_id === parentId) {
+                const newItem = item;
+                const children = createTree(arr, item.id);
+                if (children.length > 0) {
+                    newItem.children = children;
+                }
+                tree.push(newItem);
+            }
+        });
+        return tree;
+    }
+    const newCategories = createTree(categories);
+
+    console.log(newCategories)
+
     res.render("admin/pages/category/create", {
-        categories: categories,
-        categoriesTitle: categoriesTitle,
+        categories: newCategories,
         id: id
     });
 }
